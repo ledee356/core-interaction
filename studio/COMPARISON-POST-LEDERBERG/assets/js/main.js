d67c1bbda46b66884151bd53c0e370cbeba04aec
Eli Lederberg <elilederberg@Elis-MBP-2.fios-router.home>
2018-04-07 22:10:00 -0400

    compare

--- studio/COMPARISON-POST-LEDERBERG/assets/js/main.js
+++ studio/COMPARISON-POST-LEDERBERG/assets/js/main.js
@@ -1,10 +1,22 @@
-var dropbtn = document.querySelector('.dropbtn')
-var content = document.querySelector('.dropdown-content')
-dropbtn.addEventListener('mouseover', function(){
-  console.log('hello')
-  content.classList.add('.show')
+var selectOne = document.querySelector('.dropbtn.one')
+var selectTwo = document.querySelector('.dropbtn.two')
+var link = document.querySelector('.link')
+
+var selectOneValue = null
+var selectTwoValue = null
+
+function changeHref(valOne, valTwo) {
+	if (valOne && valTwo) {
+		link.href = valOne + '_' + valTwo + '.html'
+	}
+}
+
+selectOne.addEventListener('change', function() {
+	selectOneValue = selectOne.value
+	changeHref(selectOneValue, selectTwoValue)
 })
 
-dropbtn.addEventListener('mouseout', function(){
-  content.classList.remove('.show')
+selectTwo.addEventListener('change', function() {
+	selectTwoValue = selectTwo.value
+	changeHref(selectOneValue, selectTwoValue)
 })
