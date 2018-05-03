0079a9223955e52900c8b2b66a8f7fc36820abcf
Eli Lederberg <elilederberg@Elis-MBP-2.wireless.newschool.edu>
2018-05-03 11:00:15 -0400

    api

--- studio/MOVIEOFTHEWEEK-PROJECT-LEDERBERG/assets/js/main.js
+++ studio/MOVIEOFTHEWEEK-PROJECT-LEDERBERG/assets/js/main.js
@@ -7,20 +7,46 @@ video.addEventListener('mouseout', function() {
   video.pause()
 })
 
+var movieIdArray = ['tt3235888', 'tt4695012', 'tt1935179', 'tt6265828', 'tt3721936', 'tt0365748', 'tt0119396', 'tt185610', 'tt4263482', 'tt4062536', 'tt4302938', 't2235108', 'tt1133989', 'tt0418763', 'tt1020072', 'tt2562232', 'tt6217608', 'tt5742374', 'tt0209933', 'tt4846232', 'tt0462335', 'tt3464902', 'tt5325492', 'tt6303866', 'tt5962210', 'tt2334649', 'tt0120601', 't1560747', 'tt2582496', 'tt2125435', 'tt0443706']
+var date = (new Date()).getDate()
 
+console.log(date)
 
-fetch('https://www.omdbapi.com/?i=tt3896198&apikey=6d6fa250')
+// Insert your actual API request URL below
+fetch('https://www.omdbapi.com/?apikey=6d6fa250&i=' + movieIdArray[date + 1] )
     .then(function(response) {
         // Get the response and format it to JSON
         return response.json();
     })
     .then(function(jsonData) {
         // log the data
-        render(jsonData);
-
+        console.log(jsonData);
+		  render(jsonData);
     });
 
-var movieTitle = document.querySelector('.title')
-var movieDirector = document.querySelector('dir')
+
+var movieTitle = document.querySelector('.mov_title')
+var movieDirector = document.querySelector('.dir')
 var movieDescription = document.querySelector('.desc')
 var movieTrailer = document.querySelector('.video')
+
+function render(data) {
+
+  console.log(data);
+
+  console.log(data.Title);
+
+  console.log(data.Director);
+
+  console.log(data.Plot);
+
+  movieTitle.innerText = data.Title;
+
+  console.log(movieTitle);
+
+  movieDirector.innerText = data.Director;
+
+  movieDescription.innerText = data.Plot;
+
+
+}
