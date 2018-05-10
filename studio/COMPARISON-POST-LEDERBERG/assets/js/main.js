e49e412366298f72bfda426ab0df76fde1d34f48
Eli Lederberg <elilederberg@Elis-MBP-2.fios-router.home>
2018-05-10 17:42:58 -0400

    finalize

--- studio/COMPARISON-POST-LEDERBERG/assets/js/main.js
+++ studio/COMPARISON-POST-LEDERBERG/assets/js/main.js
@@ -2,21 +2,21 @@ var selectOne = document.querySelector('.dropbtn.one')
 var selectTwo = document.querySelector('.dropbtn.two')
 var link = document.querySelector('.link')
 
-var selectOneValue = null
-var selectTwoValue = null
+var selectOneId = null
+var selectTwoId = null
 
 function changeHref(valOne, valTwo) {
 	if (valOne && valTwo) {
-		link.href = valOne + '_' + valTwo + '.html'
+		link.href = 'compare.html?id1=' + selectOneId + '&id2=' + selectTwoId
 	}
 }
 
 selectOne.addEventListener('change', function() {
-	selectOneValue = selectOne.value
-	changeHref(selectOneValue, selectTwoValue)
+	selectOneId = selectOne.querySelector('option:checked').dataset.id
+	changeHref(selectOneId, selectTwoId)
 })
 
 selectTwo.addEventListener('change', function() {
-	selectTwoValue = selectTwo.value
-	changeHref(selectOneValue, selectTwoValue)
+	selectTwoId = selectTwo.querySelector('option:checked').dataset.id
+	changeHref(selectOneId, selectTwoId)
 })
